ec428ae8c0a692417eedda6ab20f369e5eba02f2
Moses Finlay <islandmoses@gmail.com>
2019-01-05 18:27:14 -0800

    Add comments

--- scraper.js
+++ scraper.js
@@ -1,8 +1,11 @@
 const date = new Date();
+
+// Creates a nicely formatted date
 const fancyDate = `${date.getFullYear()}-${date.getDate()}-${date.getMonth()+1}`;
 
 let shirtLinks = [];
 
+// CSV creator module variables
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 const csvWriter = createCsvWriter({
   path: `../data/${fancyDate}.csv`,
@@ -14,37 +17,49 @@ const csvWriter = createCsvWriter({
     { id: "time", title: "Time" },
   ]
 });
+let shirtInfo = [];
+
+// Requires the crawler module
 const Crawler = require("crawler");
 
+// Displays an error message
 function errorMessage(error) {
   console.error(`An error has occurred. Please try again. (${error.message})`);
 }
+
+// Scrapes the website for the links to each shirt
 function getShirtLinks(error, response, done) {
+  // Handles any errors
   if (error) {
-    errorMessage(error)
+    errorMessage(error);
   } else {
     const $ = response.$;
 
+    // Adds the shirt links to to shirtLinks array
     $("ul.products li a").each((index, item) => {
       shirtLinks.push(`http://shirts4mike.com/${$(item).attr("href")}`);
     });
 
+    // Runs the getShirtInfo scraper for each of the shirts
     getShirtInfoCrawler.queue(shirtLinks);
   }
   done();
 }
-let records = [];
+
+// Scrapes information like the title, price, image_url etc.
 const getShirtInfoCrawler = new Crawler({
   maxConnections: 10,
   callback: (error, response, done) => {
+    // Handles any errors
     if (error) {
       errorMessage(error)
     } else {
       const $ = response.$;
-
+      // Image url for the current shirt
       const imageURL = $("div.shirt-picture span img").attr("src");
 
-      records.push({
+      // Adds the shirt info to the CSV writer
+      shirtData.push({
         title: $("head title").text(),
         price: $("div.shirt-details h1 span").text(),
         image_url: `http://shirts4mike.com/${imageURL}`,
@@ -52,8 +67,11 @@ const getShirtInfoCrawler = new Crawler({
         time: `${fancyDate}`,
       });
 
-      if (records.length === shirtLinks.length) {
-        csvWriter.writeRecords(records)
+      // When the shirtInfo array has all of the shirt information
+      if (shirtData.length === shirtLinks.length) {
+
+        // Write the CSV file
+        csvWriter.writeRecords(shirtData)
           .then(() => console.log("Finished"))
           .catch(error => errorMessage);
       }
@@ -62,8 +80,11 @@ const getShirtInfoCrawler = new Crawler({
   }
 });
 
+// Goes to the website and gets the links to each shirt
 const getShirtLinksCrawler = new Crawler({
   maxConnections: 10,
   callback: (error, response, done) => getShirtLinks(error, response, done),
 });
+
+// Runs the getShirtLinks scraper
 getShirtLinksCrawler.queue("http://shirts4mike.com/shirts.php");
